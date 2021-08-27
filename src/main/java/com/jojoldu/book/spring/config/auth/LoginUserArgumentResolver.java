f2f47eb708b5a322ffff184b3b79e4737500cf07
Kanbot <tmdslr@gmail.com>
2021-08-27 14:52:21 +0900

    21-08-18

--- src/main/java/com/jojoldu/book/spring/config/auth/LoginUserArgumentResolver.java
+++ src/main/java/com/jojoldu/book/spring/config/auth/LoginUserArgumentResolver.java
@@ -27,6 +27,6 @@ public class LoginUserArgumentResolver implements HandlerMethodArgumentResolver 
 
     @Override
     public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
-        return httpSession.getAttribute("guest");
+        return httpSession.getAttribute("user");
     }
 }

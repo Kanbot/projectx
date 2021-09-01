9ca8a0f80562466fb8f5c1b5e067a864326ab82f
Kanbot <tmdslr@gmail.com>
2021-09-01 14:12:41 +0900

    21-09-01

--- src/main/java/com/jojoldu/book/spring/domain/posts/Posts.java
+++ src/main/java/com/jojoldu/book/spring/domain/posts/Posts.java
@@ -19,14 +19,16 @@ public class Posts extends BaseTimeEntity {
   @Column (length = 500 ,nullable = false) // 테이블의 칼럼을 나태내며 굳이 선언하지않더라도 해당 클래스의 필드는 모두 칼럼이 됩니다.
           //사용하는 이유는 기본값 외에 추가로 변경이 필요한 옵션이 있으면 사용합니다.사이즈를 늘리고 싶다던가 text로 변경하고 싶거나
     private String title;
+    private String category;
   @Column(columnDefinition = "TEXT" ,nullable = false)
     private String content;
   private String author;
   @Builder //해당 클래스의 빌더 패턴 클래스를 생성 //생성자 상단에 선언시 생성자에 포함된 필드만 빌더에 포함
-    public Posts(String title ,String content , String author){
+    public Posts(String title ,String content , String author ,String category){
       this.author =author;
       this.content = content;
       this.title =title;
+      this.category =category;
   }
   public void update(String title ,String content){
     this.title=title;
